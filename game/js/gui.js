74de3101e9c80991aa060dfb61ef107f90b1602c
Mad-Mushroom <a.no.budy1@gmail.com>
2024-01-16 18:47:56 +0100

    ich hasse mein leben

--- game/js/gui.js
+++ game/js/gui.js
@@ -80,43 +80,48 @@ class SpriteButton {
 }
 
 class AnimationButton {
-    constructor(imagePaths, SizeX, SizeY, PosX, PosY, ForeColor, BackColor, animSpeed = 0.5) {
+    constructor(imagePaths, SizeX, SizeY, PosX, PosY, FrameX, FrameY, animSpeed = 0.5) {
         this.imagePaths = imagePaths;
         this.PosX = PosX;
         this.PosY = PosY;
         this.SizeX = SizeX;
         this.SizeY = SizeY;
-        this.ForeColor = ForeColor;
-        this.BackColor = BackColor;
+        this.FrameX = FrameX;
+        this.FrameY = FrameY;
         this.animSpeed = animSpeed;
     }
 
-    Draw(){
+    async Draw(){
         this.textures = [];
 
-        // Create a texture from each image path
-        for (let i = 0; i < this.imagePaths.length; i++) {
-            let texture = PIXI.Texture.from(imagePaths[i]);
+        this.baseTexture = await PIXI.BaseTexture.from(this.imagePaths);
+
+        let width = this.FrameX;
+        let height = this.FrameY;
+
+        this.rows = Math.floor(this.baseTexture.height / height);
+        this.cols = Math.floor(this.baseTexture.width / width);
+        this.numFrames = this.rows * this.cols;
+
+        for (let i = 0; i < this.numFrames; i++) {
+            let row = Math.floor(i / this.cols);
+            let col = i % this.cols;
+            let rectangle = new PIXI.Rectangle(col * width, row * height, width, height);
+            let texture = new PIXI.Texture(this.baseTexture, rectangle);
             this.textures.push(texture);
         }
 
-        // Create the animated sprite
-        this.animatedSprite = new PIXI.AnimatedSprite(textures);
+        this.animatedSprite = new PIXI.AnimatedSprite(this.textures);
 
-        // Set the animation speed and start the animation
         this.animatedSprite.animationSpeed = this.animSpeed;
         this.animatedSprite.play();
 
-        // Change the size of the sprite
-        animatedSprite.width = this.SizeX;  // Set the width to 100 pixels
-        animatedSprite.height = this.SizeY; // Set the height to 100 pixels
+        this.animatedSprite.width = this.SizeX;
+        this.animatedSprite.height = this.SizeY;
 
-        // Change the position of the sprite
-        animatedSprite.x = this.PosX; // Set the x position to 50 pixels
-        animatedSprite.y = this.PosY; // Set the y position to 50 pixels
+        this.animatedSprite.x = this.PosX;
+        this.animatedSprite.y = this.PosY;
 
-
-        // Add the animated sprite to the stage
         app.stage.addChild(this.animatedSprite);
     }
 
